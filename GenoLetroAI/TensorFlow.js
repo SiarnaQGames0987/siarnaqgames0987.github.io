f8b543836c9fda9684f2f9209c073543dcd21cbd
SiarnaQGames0987 <mbyzt32@gmail.com>
2025-05-18 01:10:28 +0300

    Create TensorFlow.js

---
+++ GenoLetroAI/TensorFlow.js
@@ -0,0 +1,13 @@
+[
+  { input: "hello", output: "selam" },
+  { input: "merhaba", output: "selam" },
+  { input: "hi", output: "selam" },
+
+  { input: "2+2", output: "işlem" },
+  { input: "10*3", output: "işlem" },
+  { input: "50/10", output: "işlem" },
+
+  { input: "write code", output: "engelle" },
+  { input: "generate c#", output: "engelle" },
+  { input: "python code", output: "engelle" }
+]
